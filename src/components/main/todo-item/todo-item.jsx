76b02b7873a6c7c5470e03abbb66262e937e1abc
shahirov <arafat.shakhirov@gmail.com>
2019-10-20 20:48:10 +0500

    chore(components): add todo-list and todo-item components

---
+++ src/components/main/todo-item/todo-item.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TodoItem = () => {
+  return (
+    <li>Drink Coffee</li>
+  );
+};
+
+export default TodoItem;
---
+++ src/components/main/todo-list/todo-list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TodoItem from '../todo-item/todo-item';
+
+const TodoList = () => {
+  return (
+    <ul>
+      <TodoItem />
+    </ul>
+  );
+};
+
+export default TodoList;
