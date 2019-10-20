06412c2170855685337adc9a135c852147f6eaff
shahirov <arafat.shakhirov@gmail.com>
2019-10-20 20:06:14 +0500

    chore(item-add-form): create item-add-form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/header/header';
+import ItemAddForm from './components/item-add-form/item-add-form';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <ItemAddForm />
     </div>
   );
 };
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,9 +4,9 @@ import styles from './header.module.css';
 
 const Header = () => {
   return (
-    <header>
+    <>
       <h1 className={styles.title}>todos</h1>
-    </header>
+    </>
   );
 };
 
