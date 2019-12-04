55a9c5bc2e9d866ae319e882a43a99e23292e853
shahirov <arafat.shakhirov@gmail.com>
2019-12-05 02:29:35 +0500

    Updates

---
+++ precache-manifest.516a23a957dcd3defbec9fdac8f8b733.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18caebd3f6240c8680b16fcdf57c7971",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "30250801329bd157e796",
+    "url": "/react-todo/static/css/main.8e6334dd.chunk.css"
+  },
+  {
+    "revision": "ad3802abd5e0be740dcb92ffc5b63805",
+    "url": "/react-todo/static/fonts/roboto-bold-webfont.ad3802ab.woff2"
+  },
+  {
+    "revision": "9216bc30a32de4e241849e4f4ffdf09b",
+    "url": "/react-todo/static/fonts/roboto-medium-webfont.9216bc30.woff2"
+  },
+  {
+    "revision": "8174860a69e035b4f3cdd2067dab8abd",
+    "url": "/react-todo/static/fonts/roboto-regular-webfont.8174860a.woff2"
+  },
+  {
+    "revision": "ab9db8c89f0492851d80",
+    "url": "/react-todo/static/js/chunk-vendors.9fedbf67.chunk.js"
+  },
+  {
+    "revision": "e54dc1389c32a7bb9d610582a6253a13",
+    "url": "/react-todo/static/js/chunk-vendors.9fedbf67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30250801329bd157e796",
+    "url": "/react-todo/static/js/main.e1ccdc49.chunk.js"
+  },
+  {
+    "revision": "227b198651d364063349",
+    "url": "/react-todo/static/js/runtime~main.1de0b5a2.js"
+  }
+]);
